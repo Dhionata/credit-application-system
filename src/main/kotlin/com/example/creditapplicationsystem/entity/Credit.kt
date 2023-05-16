7b6435be428fcc683bdabf31e63e6851ca3cf30b
Dhionatã <xiyunagi@gmail.com>
2023-05-15 21:23:52 -0300

    Criação das Classes Credit, Customer e Address; Criação do Enum Status e dos repositórios CreditRepository e CustomerRepository. Criação da migration v1(table_customer) e v2(table_credit), bem como a criação da application.yml.

---
+++ src/main/kotlin/com/example/creditapplicationsystem/entity/Credit.kt
@@ -0,0 +1,19 @@
+package com.example.creditapplicationsystem.entity
+
+import com.example.creditapplicationsystem.enummeration.Status
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.*
+
+@Entity
+@Table
+data class Credit(
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstIntallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int,
+    @Enumerated val status: Status = Status.IN_PROGRESS,
+    @ManyToOne val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long?
+)
---
+++ src/main/kotlin/com/example/creditapplicationsystem/repository/CustomerRepository.kt
@@ -0,0 +1,8 @@
+package com.example.creditapplicationsystem.repository
+
+import com.example.creditapplicationsystem.entity.Customer
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository : JpaRepository<Customer, Long>
